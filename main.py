5b914e5e64e39f899825cf8959cb54e4cd2e038f
Peter Kassouf <pk145005@gmail.com>
2025-05-27 01:15:38 +0000

    Move analysis router after app instantiation

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import logging
 
 from app.api import summary as summary_api
 from app.api import huddle_ui
+from app.api import event as event_api
+from app.api.analysis import router as analysis_router
 
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI(title="BCI Core")
+
 app.include_router(huddle_ui.router)
+app.include_router(event_api.router)
 app.include_router(summary_api.router)
+app.include_router (analysis_router)
 
 DEFAULT_ERROR = "TEST_ERROR_ALERT"
 
